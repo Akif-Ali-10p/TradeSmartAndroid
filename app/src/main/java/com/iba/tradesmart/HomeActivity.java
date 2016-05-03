19b01a04d523ebc6bf25088818eaafbdbbd9ff1e
Firdous <firdous.ali86@gmail.com>
2016-05-03 14:27:53 +0500

    button

--- app/src/main/java/com/iba/tradesmart/HomeActivity.java
+++ app/src/main/java/com/iba/tradesmart/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.iba.tradesmart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Button feedbackViewBtn;
+
+    private void setupButtons(){
+        feedbackViewBtn = (Button) findViewById(R.id.feedbackbtn);
+
+        feedbackViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, FeedActivity.class));
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +55,8 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setupButtons();
     }
 
     @Override
