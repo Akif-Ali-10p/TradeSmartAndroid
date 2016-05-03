1cc4b0b0f1bb3944fe49506c5edf458b61dfbd4a
Firdous <firdous.ali86@gmail.com>
2016-05-03 16:07:27 +0500

    download intent

--- app/src/main/java/com/iba/tradesmart/FeedActivity.java
+++ app/src/main/java/com/iba/tradesmart/FeedActivity.java
@@ -1,18 +1,25 @@
 package com.iba.tradesmart;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.tenpearls.android.activities.BaseActivity;
+import com.tenpearls.android.components.TextView;
 import com.tenpearls.android.interfaces.Controller;
 import com.tenpearls.android.service.ServiceFactory;
 import com.tenpearls.android.views.BaseView;
 
 public class FeedActivity extends BaseActivity {
+
+    private static final String URL = "http://www.skysports.com/rss/0,20514,11661,00.xml";
+    private static final int RSS_DOWNLOAD_REQUEST_CODE = 0;
 
     @Override
     public BaseView getViewForController(Controller controller) {
@@ -22,6 +29,13 @@ public class FeedActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PendingIntent pendingResult = createPendingResult(
+                RSS_DOWNLOAD_REQUEST_CODE, new Intent(), 0);
+        Intent intent = new Intent(getApplicationContext(), DownloadIntentService.class);
+        intent.putExtra(DownloadIntentService.URL_EXTRA, URL);
+        intent.putExtra(DownloadIntentService.PENDING_RESULT_EXTRA, pendingResult);
+        startService(intent);
     }
 
     @Override
@@ -29,4 +43,42 @@ public class FeedActivity extends BaseActivity {
         return null;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RSS_DOWNLOAD_REQUEST_CODE) {
+            switch (resultCode) {
+                case DownloadIntentService.INVALID_URL_CODE:
+                    handleInvalidURL();
+                    break;
+                case DownloadIntentService.ERROR_CODE:
+                    handleError(data);
+                    break;
+                case DownloadIntentService.RESULT_CODE:
+                    handleRSS(data);
+                    break;
+            }
+            handleRSS(data);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleRSS(Intent data) {
+        IllustrativeRSS rss = data.getParcelableExtra(DownloadIntentService.RSS_RESULT_EXTRA);
+//        ViewGroup result = (ViewGroup)findViewById(R.id.results);
+//        result.removeAllViews();
+//        for (int i=0; i<rss.size(); i++) {
+//            IllustrativeRSS.Item item = rss.get(i);
+//            TextView v = new TextView(this);
+//            v.setText(item.title);
+//            result.addView(v);
+//        }
+    }
+
+    private void handleError(Intent data) {
+        // whatever you want
+    }
+
+    private void handleInvalidURL() {
+        // whatever you want
+    }
 }
--- app/src/main/java/com/iba/tradesmart/FeedItem.java
+++ app/src/main/java/com/iba/tradesmart/FeedItem.java
@@ -3,12 +3,27 @@ package com.iba.tradesmart;
 import com.google.gson.JsonElement;
 import com.tenpearls.android.entities.BaseEntity;
 
+import java.util.Date;
+
 /**
  * Created by firdous on 03/05/16.
  */
 public class FeedItem extends BaseEntity {
     private String title;
     private String thumbnail;
+    private String url;
+    private String description;
+    private Date pubDate;
+
+    public FeedItem() {
+    }
+
+    public FeedItem(String url, String title, String description, String imageUrl) {
+        this.url = url;
+        this.title = title;
+        this.description = description;
+        this.thumbnail = imageUrl;
+    }
 
     public String getTitle() {
         return title;
@@ -24,6 +39,29 @@ public class FeedItem extends BaseEntity {
 
     public void setThumbnail(String thumbnail) {
         this.thumbnail = thumbnail;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDesc(String description) {
+        this.description = description;
+    }
+
+    public void setpubDate(Date pubDate){
+        this.pubDate = pubDate;
+    }
+    public Date getpubDate(){
+        return pubDate;
     }
 
     @Override
